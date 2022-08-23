276b7b198353b6c2e4e0d16d9ebedd69040633e6
John <jmmcmaster09@gmail.com>
2022-08-22 20:55:31 -0500

    Commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'My Favorite Photos';
+  image1 = 'https://helios-i.mashable.com/imagery/articles/03oOVkx2zJp6HNKSkewdKIX/hero-image.fill.size_1200x900.v1623362910.jpg';
+  image2 = 'https://media.istockphoto.com/photos/cute-wild-animal-loves-his-freedom-picture-id1136409899?k=20&m=1136409899&s=612x612&w=0&h=FxaUojNml9ijAqbwBN29teuQi84tbGD2KEI7iBQK9Hw=';
+  image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX7Gnjb2hxZfXcJvCSQTCn1bHJwDUnUOby6t02W_Al&s';
 
   constructor() { }
 
